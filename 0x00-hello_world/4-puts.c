649d3566912eb2f1a9d1b447583fdcdeec166d6d
Benterf123 <faithbenter@gmail.com>
2022-06-02 13:32:12 -0700

    C program that prints given line, followed by new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <studio.h>
 
 /**
- * main - Entry point
- *
+ * main - void enables the code to run successfully
+(*
  * Return: Always 0 (Success)
+ *Description: printing using puts)?
+ *Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0) ;
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0) ;}
