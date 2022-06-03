f53fd87474c15edcd8299209aeaafb13f3ca1672
Benterf123 <faithbenter@gmail.com>
2022-06-03 13:22:00 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program that prints the alphabets in lowercases
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets;
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	
+	return (0);
+}
