85746c4242e051436719caefe8b5eb3a665dd468
Benterf123 <faithbenter@gmail.com>
2022-06-03 14:56:07 -0700

    Lowercase numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing all numbers of base 16
+ * Description: program that prints all lowercase numbersin base 16)?
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <='9'; digit++)
+		putchar(digit);
+
+	for (digit = 'a'; digit <='f'; digit++)
+		putchar(digit);
+	
+	putchar('\n');
+
+	return(0);
+}
+
