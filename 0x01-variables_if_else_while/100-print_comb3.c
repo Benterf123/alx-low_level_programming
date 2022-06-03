c2aa9d9cab07c6cec758339d272313bf5ca25a1f
Benterf123 <faithbenter@gmail.com>
2022-06-03 16:04:20 -0700

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible digit combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int tents;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('n\');
+
+	return (0);
+}
