3ab79f4938b5369660cf133955381d2d072fe92f
Benterf123 <faithbenter@gmail.com>
2022-06-03 11:09:17 -0700

    Positive anything is better than negative anything

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - program that assigns a random number to a variable each time it is executed
+ *
+ * main - program that assigns a random number to a variable and executes it
  *
  * Return: 0
  */
@@ -11,14 +12,14 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand (time(0));
 	n = rand() - RAND_MAX/2;
 
-	if (n==0)
-		printf("%d is zeo\n", n);
-	else if (n<0)
+	if(n==0)
+		printf("%d is zero\n", n);
+	else if(n<0)
 		printf("%d is negative\n", n);
-	else
+	else 
 		printf("%d is positive\n", n);
 
 	return (0);
