f3398a5b56928756ce9fe14b51c9f4bf3e431ef0
Benterf123 <faithbenter@gmail.com>
2022-06-03 13:01:59 -0700

    Printing alphabets in lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio,h>
+
+/**
+ * main - program that prints lowercase alphabets
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c)
+		putchar('\n');
+
+return (0);
+}
