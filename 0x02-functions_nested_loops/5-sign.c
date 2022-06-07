76cd78eea92c4796b9defb3e5b4bb05a47083cee
Benterf123 <faithbenter@gmail.com>
2022-06-07 13:13:31 -0700

    Printing signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@
 
 int print_sign(int n)
 {
-	if (n > 1)
+	if (n < 0)
 	{
-		return (1);
+		return (-);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		return (0);
 	}
 	else 
 	{
-		return (-);
+		return (1);
 	}
 }
