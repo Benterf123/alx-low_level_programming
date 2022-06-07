04a42bdb4d263be2cd6cc354c7a6dcd38dcfb02a
Benterf123 <faithbenter@gmail.com>
2022-06-07 10:45:58 -0700

    Printing 10 times lowercase alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,12 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 
 	for (round = 0; round < 10; round++)
+	{
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
 	_putchar('\n');
+	}
 }
