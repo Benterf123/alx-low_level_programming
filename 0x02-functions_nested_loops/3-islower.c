b157c208f4199febb73af4ae6bc5d821df77911d
Benterf123 <faithbenter@gmail.com>
2022-06-07 11:42:42 -0700

    Detecting lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: program that checks for lowercase characters
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	char c = 'a';
+
+	if (c = 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
