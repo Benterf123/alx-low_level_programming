b08d84a301ee30a8036d2c66db6def818c76ce65
Benterf123 <faithbenter@gmail.com>
2022-06-07 06:49:02 -0700

    Printing lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  *
  * Return: 0
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(letter);
+		putchar(letter);
 	}
 	putchar('\n');
 
