31db4cf7178425105d2895c3dd207e2b0469df65
Benterf123 <faithbenter@gmail.com>
2022-06-07 07:32:04 -0700

    Printing lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints lowercase alphabets
+ * print_alphabet - prints lowercase alphabets
  * Description: putchar function that prints lowercase alphabets 
  *
  * Return: void
