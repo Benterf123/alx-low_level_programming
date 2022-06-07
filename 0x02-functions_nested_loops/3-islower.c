8a37eef96bef30070af5d8ca0d606e2f645e1527
Benterf123 <faithbenter@gmail.com>
2022-06-07 12:10:35 -0700

    Detecting lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * _islower - checks for lowercase characters
  * @c: program that checks for lowercase characters
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is lowercase
  */
 
 int _islower(int c)
