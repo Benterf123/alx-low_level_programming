4327a136ba64780bd2d51dbed65be8f5b49ee1c7
Benterf123 <faithbenter@gmail.com>
2022-06-07 16:18:42 -0700

    Times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,40 +8,19 @@
  */
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	int i, j;
+	i = 0;
 
-	hours max = 58;
-	hours tens = '0';
-
-	while (hours_tens == '3')
+	for (i <= 24, i = 0; i++)
 	{
-		if (hours_tens == '2')
-		{
-			hours_max = '4';
-		}
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_tens < '6')
-			{
-				minutes_ones = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-			minutes_tens = '0';
-			hours_ones++;
-		}
-		hours_ones = '0';
-		hours_tens++;
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		putchar (':');
+	for (j <= 60; j = 0; j++)
+	{
+		_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
+		_putchar('\n');
+	}
 	}
 }
