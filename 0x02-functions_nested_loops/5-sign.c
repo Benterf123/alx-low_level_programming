f43f90f70bd740213cfade25a6d809ed79d9c17f
Benterf123 <faithbenter@gmail.com>
2022-06-07 13:31:18 -0700

    Printing signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,21 +4,24 @@
  * print_sign - prints signs of numbers
  *@n: program that prints the signs of numbers
  *
- * Return: 1 if n is greater than 0, 0 if n is 0 and - if n is less than 0
+ * Return: 1 if n is greater than 0, 0 if n is 0 and -1 if n is less than 0
  */
 
 int print_sign(int n)
 {
 	if (n < 0)
 	{
+		_putchar(-1)
 		return ('-');
 	}
 	else if (n == 0)
 	{
+		_putchar(0);
 		return (0);
 	}
 	else
 	{
+		_putchar(+);
 		return (1);
 	}
 }
