da6bf2a57d38e3d36c690109f55d219ef5f3fe71
Benterf123 <faithbenter@gmail.com>
2022-06-07 17:05:28 -0700

    Printing minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,49 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints jack bauer's daily minutes
- * Description: program that prints jack bauer's minutes of the day
+ * jack_bauer - prints every minute of the day
+ *
+ * Description: prints every minute of the day
  *
  * Return: void
  */
+
 void jack_bauer(void)
 {
-	int i, j;
-	i = 0;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i <= 24, i = 0; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		putchar (':');
-	for (j <= 60; j = 0; j++)
-	{
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-	}
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
