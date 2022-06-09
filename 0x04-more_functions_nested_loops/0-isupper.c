c3f453dd39c5092d6a64358a4bfef43030b8ba7e
Benterf123 <faithbenter@gmail.com>
2022-06-09 11:07:47 -0700

    Detecting lowercase and uppercase alphabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks the code for uppercase characters
+ * @c: program that checks for uppercase characters
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+	char c;
+
+	if (c = 'a')
+		return (0);
+	if (c = 'A')
+		return (1);
+
+	return (0);
+}
