0eff6e5deedcd2132679314c07bffd80cd7cffae
Benterf123 <faithbenter@gmail.com>
2022-06-30 13:48:39 -0700

    Malloc memory allocation

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -5,3 +8,11 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _putchar(char c);
 
+#include <stdlib.h>
+#include <stdio.h>
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
