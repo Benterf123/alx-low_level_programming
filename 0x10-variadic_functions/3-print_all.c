4e68bdc1706aa967fe9cb8b66c04f6cc6c2e9333
Benterf123 <faithbenter@gmail.com>
2022-07-07 08:49:07 -0700

    Printing anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,9 +1,7 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
- *
  * Return: no return.
  */
 void print_all(const char * const format, ...)
@@ -27,6 +25,12 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
 		case 'f':
 			printf("%f", va_arg(valist, double)), c = 1;
 			break;
@@ -41,7 +45,5 @@ void print_all(const char * const format, ...)
 			break;
 		} i++;
 	}
-
 	printf("\n"), va_end(valist);
-
 }
